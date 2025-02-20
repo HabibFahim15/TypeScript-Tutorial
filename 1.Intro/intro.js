d96388edd402a0fed5a165dc50c8b3a6b8adc0fd
HabibFahim15 <habibfahim71@gmail.com>
2025-02-21 01:14:42 +0600

    first commit

---
+++ 1.Intro/intro.js
@@ -0,0 +1,4 @@
+var user = { name: 'habib', age: 23 };
+console.log('Habib Hossain');
+console.log(user.name);
+// console.log(user.email);
---
+++ 1.Intro/intro.ts
@@ -0,0 +1,5 @@
+const user = {name: 'habib', age: 23};
+
+console.log('Habib Hossain');
+console.log(user.name);
+// console.log(user.email);
